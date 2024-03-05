19dc7d036ee87cc8e8dc966f2ab8ea87beb7d38d
k <developerlyk@gmail.com>
2024-03-05 11:42:14 +0900

    Test 추가

---
+++ src/test/java/hello/hellospring/repositroy/MemoryMemberRepositoryTest.java
@@ -0,0 +1,25 @@
+package hello.hellospring.repositroy;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MemoryMemberRepositoryTest {
+
+    MemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        assertThat(member).isEqualTo(result);
+    }
+
+}
